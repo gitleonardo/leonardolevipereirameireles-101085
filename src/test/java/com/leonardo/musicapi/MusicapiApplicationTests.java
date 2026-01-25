e7a6b56beca94bc300ec96305d1f0fc8538c9b23
Leonardo P Meireles <br.com.leonardo@gmail.com>
2026-01-25 13:51:26 -0300

    chore: bootstrap do projeto Spring Boot com perfil de teste isolado

---
+++ src/test/java/com/leonardo/musicapi/MusicapiApplicationTests.java
@@ -0,0 +1,9 @@
+package com.leonardo.musicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.profiles.active=test")
+class MusicapiApplicationTests {
+  @Test void contextLoads() {}
+}
